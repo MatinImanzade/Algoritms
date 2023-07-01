e2b4ceb6b018de2372c08212a3fdd7c8240c2544
Matin Imanzade <matinimanzade@gmail.com>
2023-07-01 19:02:07 +0400

    initial commit

--- src/coders/iyun24/FindPolyndrom.java
+++ src/coders/iyun24/FindPolyndrom.java
@@ -5,13 +5,13 @@ import java.util.Arrays;
 import java.util.List;
 
 public class FindPolyndrom {
-    public static void main(String[] args) {
-        List<String> stringList = Arrays.asList("SOS", "Tavat", "asd");
-        int last = stringList.size()-1;
-        for (int i = 0;i<stringList.size();i++){
-            stringList.stream()
-                    .filter(n-> n.charAt(i) == n.charAt(last));
-        }
-
-    }
+//    public static void main(String[] args) {
+//        List<String> stringList = Arrays.asList("SOS", "Tavat", "asd");
+//        int last = stringList.size()-1;
+//        for (int i = 0;i<stringList.size();i++){
+//            stringList.stream()
+//                    .filter(n-> n.charAt(i) == n.charAt(last));
+//        }
+//
+//    }
 }
